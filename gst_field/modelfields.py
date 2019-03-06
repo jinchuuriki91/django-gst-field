5efaebeec2c71e9fffb6ffa12f5db3f0a370ce89
Gandhar Pednekar <gandhar.pednekar15@gmail.com>
2019-03-06 20:45:08 +0530

    default validators added to model fields

--- gst_field/modelfields.py
+++ gst_field/modelfields.py
@@ -8,10 +8,11 @@ from django.utils.translation import ugettext_lazy as _
 # Project imports
 from gst_field.constants import GSTIN_MAX_LENGTH, PAN_MAX_LENGTH
 from gst_field import formfields
+from gst_field.validators import gstin_check, pan_check
 
 
 class GSTField(CharField):
-
+    default_validators = [gstin_check]
     description = _("Goods and Services Tax")
 
     def __init__(self, *args, **kwargs):
@@ -26,7 +27,7 @@ class GSTField(CharField):
 
 
 class PANField(CharField):
-
+    default_validators = [pan_check]
     description = _("Permanent account number")
 
     def __init__(self, *args, **kwargs):
