9d1c5c1fa9f3ec31862736ba0c561cdafaed25d7
Gandhar Pednekar <gandhar.pednekar15@gmail.com>
2019-03-06 20:44:12 +0530

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-gst-field',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/jinchuuriki91/django-gst-field',
     license='MIT',
     platforms=['OS Independent'],
